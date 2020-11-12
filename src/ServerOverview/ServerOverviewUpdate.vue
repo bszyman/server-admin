8c5c80b8649fdfa5b983d205c0f94232e30718d8
Ben Szymanski <bszyman@icloud.com>
2020-11-12 00:29:35 -0500

    Basic routing + proof implementing system overview section

---
+++ src/ServerOverview/ServerOverviewUpdate.vue
@@ -0,0 +1,28 @@
+<template>
+  <sui-grid>
+    <sui-grid-row>
+      <sui-grid-column :width="16">
+        <sui-menu>
+          <sui-menu-item><router-link :to="{ name: 'server-overview', params: {}}">Overview</router-link></sui-menu-item>
+          <sui-menu-item><router-link :to="{ name: 'server-logs', params: {}}">Logs</router-link></sui-menu-item>
+          <sui-menu-item><router-link :to="{ name: 'server-system', params: {}}">System</router-link></sui-menu-item>
+          <sui-menu-item><router-link :to="{ name: 'server-graphs', params: {}}">Graphs</router-link></sui-menu-item>
+          <sui-menu-item active>Update</sui-menu-item>
+          <sui-menu-item><router-link :to="{ name: 'server-settings', params: {}}">Settings</router-link></sui-menu-item>
+        </sui-menu>
+      </sui-grid-column>
+    </sui-grid-row>
+    <sui-grid-row>
+    </sui-grid-row>
+  </sui-grid>
+</template>
+
+<script>
+export default {
+  name: "ServerOverviewStart"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,65 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import SemanticUI from "semantic-ui-vue";
+import 'semantic-ui-css/semantic.min.css';
+import ServerOverview from "@/ServerOverview/ServerOverview";
+import ServerOverviewStart from "@/ServerOverview/ServerOverviewStart";
+import ServerOverviewLogs from "@/ServerOverview/ServerOverviewLogs";
+import ServerOverviewSystem from "@/ServerOverview/ServerOverviewSystem";
+import ServerOverviewGraphs from "@/ServerOverview/ServerOverviewGraphs";
+import ServerOverviewUpdate from "@/ServerOverview/ServerOverviewUpdate";
+import ServerOverviewSettings from "@/ServerOverview/ServerOverviewSettings";
 
 Vue.config.productionTip = false
 
+Vue.use(SemanticUI);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {
+      path: '/',
+      component: ServerOverview,
+      children: [
+        {
+          path: '',
+          component: ServerOverviewStart,
+          name: "server-overview",
+        },
+        {
+          path: 'logs/',
+          component: ServerOverviewLogs,
+          name: "server-logs",
+        },
+        {
+          path: 'system/',
+          component: ServerOverviewSystem,
+          name: "server-system",
+        },
+        {
+          path: 'graphs/',
+          component: ServerOverviewGraphs,
+          name: "server-graphs",
+        },
+        {
+          path: 'update/',
+          component: ServerOverviewUpdate,
+          name: "server-update",
+        },
+        {
+          path: 'settings/',
+          component: ServerOverviewSettings,
+          name: "server-settings",
+        },
+      ]
+    },
+  ]
+});
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
