3da2626ad0946ca75078faf4e6328e564e4ea61e
Ben Szymanski <bszyman@icloud.com>
2020-11-30 18:23:53 -0600

    Centering text on server overview panel

--- src/ServerOverview/ServerOverviewStart.vue
+++ src/ServerOverview/ServerOverviewStart.vue
@@ -11,28 +11,28 @@
           <sui-table compact basic="very">
             <sui-table-body>
               <sui-table-row>
-                <sui-table-cell>System Version:</sui-table-cell>
-                <sui-table-cell>Mac OS X 10.3.4 (Build 7H63)</sui-table-cell>
+                <sui-table-cell :width="8" text-align="right">System Version:</sui-table-cell>
+                <sui-table-cell :width="8">Mac OS X 10.3.4 (Build 7H63)</sui-table-cell>
               </sui-table-row>
               <sui-table-row>
-                <sui-table-cell>Server Version:</sui-table-cell>
-                <sui-table-cell>Mac OS X Server 10.3.4 (Build 7H63)</sui-table-cell>
+                <sui-table-cell :width="8" text-align="right">Server Version:</sui-table-cell>
+                <sui-table-cell :width="8">Mac OS X Server 10.3.4 (Build 7H63)</sui-table-cell>
               </sui-table-row>
               <sui-table-row>
-                <sui-table-cell>Computer Name:</sui-table-cell>
-                <sui-table-cell>Cocos</sui-table-cell>
+                <sui-table-cell :width="8" text-align="right">Computer Name:</sui-table-cell>
+                <sui-table-cell :width="8">Cocos</sui-table-cell>
               </sui-table-row>
               <sui-table-row>
-                <sui-table-cell>Rendezvous Name:</sui-table-cell>
-                <sui-table-cell>Cocos.local</sui-table-cell>
+                <sui-table-cell :width="8" text-align="right">Rendezvous Name:</sui-table-cell>
+                <sui-table-cell :width="8">Cocos.local</sui-table-cell>
               </sui-table-row>
               <sui-table-row>
-                <sui-table-cell>Default AppleTalk Zone:</sui-table-cell>
-                <sui-table-cell>Unspecified</sui-table-cell>
+                <sui-table-cell :width="8" text-align="right">Default AppleTalk Zone:</sui-table-cell>
+                <sui-table-cell :width="8">Unspecified</sui-table-cell>
               </sui-table-row>
               <sui-table-row>
-                <sui-table-cell>License Type:</sui-table-cell>
-                <sui-table-cell>Unlimited-client license</sui-table-cell>
+                <sui-table-cell :width="8" text-align="right">License Type:</sui-table-cell>
+                <sui-table-cell :width="8">Unlimited-client license</sui-table-cell>
               </sui-table-row>
             </sui-table-body>
           </sui-table>
