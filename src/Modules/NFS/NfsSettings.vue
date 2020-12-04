3889033be7c88c3ecde7030e72d39acd760d7f8d
Ben Szymanski <bszyman@icloud.com>
2020-12-04 00:34:33 -0600

    Implementing remaining NFS module layouts

--- src/Modules/NFS/NfsSettings.vue
+++ src/Modules/NFS/NfsSettings.vue
@@ -9,7 +9,29 @@
       <sui-grid-column :width="16">
         <sui-tab>
           <sui-tab-pane title="General">
-            <h3>General</h3>
+            <sui-form>
+              <sui-form-field inline>
+                <label for="server-deamons">Use</label>
+                <input id="server-deamons" type="number" value="6" />
+                <span style="margin-left: 10px;">server daemons</span>
+              </sui-form-field>
+
+              <sui-form-fields grouped>
+                <label for="server-clients-via">Serve clients via:</label>
+                <div style="margin-left: 25px;">
+                  <sui-form-field>
+                    <sui-checkbox radio name="server-clients-via" label="TCP and UDP" value="1" />
+                  </sui-form-field>
+                  <sui-form-field>
+                    <sui-checkbox radio name="server-clients-via" label="TCP" value="2" />
+                  </sui-form-field>
+                  <sui-form-field>
+                    <sui-checkbox radio name="server-clients-via" label="UDP" value="3" />
+                  </sui-form-field>
+                </div>
+              </sui-form-fields>
+
+            </sui-form>
           </sui-tab-pane>
         </sui-tab>
       </sui-grid-column>
