1c84f9b48d78413ec8399948b6944989ef26c1c0
Ben Szymanski <bszyman@icloud.com>
2020-12-04 00:25:56 -0600

    Implementing remaining NAT module layouts

--- src/Modules/NAT/NatSettings.vue
+++ src/Modules/NAT/NatSettings.vue
@@ -9,7 +9,18 @@
       <sui-grid-column :width="16">
         <sui-tab>
           <sui-tab-pane title="General">
-            <h3>General</h3>
+            <sui-form>
+              <sui-form-field inline>
+                <label for="encoding-option">Encoding for older clients:</label>
+                <sui-dropdown
+                    id="encoding-option"
+                    placeholder="Authentication Method"
+                    selection
+                    :options="encodingTypes"
+                    v-model="selectedEncodingType"
+                />
+              </sui-form-field>
+            </sui-form>
           </sui-tab-pane>
         </sui-tab>
       </sui-grid-column>
@@ -21,7 +32,17 @@
 import NatMenu from "@/Modules/NAT/Components/NatMenu";
 export default {
   name: "NatSettings",
-  components: {NatMenu}
+  components: {NatMenu},
+  data() {
+    let encodingTypes = [
+      { text: "Built-in Ethernet", value: 1 }
+    ];
+
+    return {
+      encodingTypes: encodingTypes,
+      selectedEncodingType: 1
+    }
+  }
 }
 </script>
 
