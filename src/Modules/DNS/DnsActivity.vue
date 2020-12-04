d0dad17778a11322aad9603b47a60ced3be04ebd
Ben Szymanski <bszyman@icloud.com>
2020-12-03 23:28:56 -0600

    Implementing more of the DNS module layouts

--- src/Modules/DNS/DnsActivity.vue
+++ src/Modules/DNS/DnsActivity.vue
@@ -7,7 +7,23 @@
     </sui-grid-row>
     <sui-grid-row>
       <sui-grid-column :width="16">
+        <p style="text-align: center;">Server Reloaded: Date and time not available</p>
 
+        <sui-table compact basic="very">
+          <sui-table-body>
+            <sui-table-row>
+              <sui-table-cell :width="8" text-align="right">SOA Queries in Progress:</sui-table-cell>
+              <sui-table-cell :width="8">0</sui-table-cell>
+            </sui-table-row>
+            <sui-table-row>
+              <sui-table-cell :width="8" text-align="right">Zone Transfers Running:</sui-table-cell>
+              <sui-table-cell :width="8">Off</sui-table-cell>
+            </sui-table-row>
+          </sui-table-body>
+        </sui-table>
+
+        <p>Current Queries</p>
+        <sui-segment></sui-segment>
       </sui-grid-column>
     </sui-grid-row>
   </sui-grid>
--- src/Modules/DNS/DnsSettings.vue
+++ src/Modules/DNS/DnsSettings.vue
@@ -9,13 +9,41 @@
       <sui-grid-column :width="16">
         <sui-tab>
           <sui-tab-pane title="General">
-            <h3>General</h3>
+            <sui-form>
+              <sui-form-field>
+                <sui-checkbox label="Allow Zone Transfers" />
+              </sui-form-field>
+              <sui-form-field>
+                <sui-checkbox label="Allow Recursion" />
+              </sui-form-field>
+            </sui-form>
           </sui-tab-pane>
           <sui-tab-pane title="Zones">
             <h3>Zones</h3>
           </sui-tab-pane>
           <sui-tab-pane title="Logging">
-            <h3>Logging</h3>
+            <sui-form>
+              <sui-form-field inline>
+                <label for="log-level">Log Level:</label>
+                <sui-dropdown
+                    id="log-level"
+                    placeholder="Select Level"
+                    selection
+                    :options="logDetailOptions"
+                    v-model="selectedLogDetailLevel"
+                />
+              </sui-form-field>
+              <sui-form-field inline>
+                <label for="log-path">Log Path:</label>
+                <input id="log-path" type="text" />
+                <sui-button
+                    type="button"
+                    icon="ellipsis horizontal"
+                    size="small"
+                    style="margin-left: 10px;"
+                />
+              </sui-form-field>
+            </sui-form>
           </sui-tab-pane>
         </sui-tab>
       </sui-grid-column>
@@ -27,7 +55,46 @@
 import DnsMenu from "@/Modules/DNS/Components/DnsMenu";
 export default {
   name: "DnsSettings",
-  components: {DnsMenu}
+  components: {DnsMenu},
+  data() {
+    const logDetailOptions = [
+      {
+        key: 1,
+        text: "Critical",
+        value: 1
+      },
+      {
+        key: 2,
+        text: "Error",
+        value: 2
+      },
+      {
+        key: 3,
+        text: "Warning",
+        value: 3
+      },
+      {
+        key: 4,
+        text: "Notice",
+        value: 4
+      },
+      {
+        key: 5,
+        text: "Information",
+        value: 5
+      },
+      {
+        key: 6,
+        text: "Debug",
+        value: 6
+      }
+    ];
+
+    return {
+      selectedLogDetailLevel: 5,
+      logDetailOptions: logDetailOptions
+    }
+  }
 }
 </script>
 
