295867c9fed80fda991788630627d9060879a967
Ben Szymanski <bszyman@icloud.com>
2020-12-05 00:42:38 -0600

    Complete netboot module layouts

--- src/Modules/NetBoot/NetbootSettings.vue
+++ src/Modules/NetBoot/NetbootSettings.vue
@@ -9,13 +9,54 @@
       <sui-grid-column :width="16">
         <sui-tab>
           <sui-tab-pane title="General">
-            <h3>General</h3>
+            <h4>Enable Netboot on at least one port.</h4>
+            <sui-segment></sui-segment>
+
+            <h4>Select where to put images and client data.</h4>
+            <sui-segment></sui-segment>
           </sui-tab-pane>
           <sui-tab-pane title="Images">
-            <h3>Images</h3>
+            <h4>Enable images and select a default image.</h4>
+            <sui-segment></sui-segment>
+
+            <h4>Image Info</h4>
+            <sui-segment></sui-segment>
           </sui-tab-pane>
           <sui-tab-pane title="Filters">
-            <h3>Filters</h3>
+            <sui-form>
+              <sui-form-field>
+                <sui-checkbox label="Enable NetBoot filtering" />
+              </sui-form-field>
+
+              <div style="margin-left: 50px;">
+                <sui-form-fields grouped>
+                  <sui-form-field>
+                    <sui-checkbox radio label="Allow only clients listed below (deny others)" value="1" />
+                  </sui-form-field>
+                  <sui-form-field>
+                    <sui-checkbox radio label="Deny only clients listed below (allow others)" value="1" />
+                  </sui-form-field>
+                </sui-form-fields>
+
+                <sui-segment></sui-segment>
+                <div is="sui-button-group" style="margin-bottom: 15px;">
+                  <sui-button icon="plus" compact />
+                  <sui-button icon="minus" compact />
+                </div>
+              </div>
+
+              <sui-form-field inline>
+                <label for="host-name">Host Name</label>
+                <input id="host-name" type="text" />
+                <sui-button icon="search" style="margin-left: 10px;" compact />
+              </sui-form-field>
+
+              <sui-form-field inline>
+                <label for="ethernet-address">Ethernet Address</label>
+                <input id="ethernet-address" type="text" value="" />
+                <sui-button icon="plus" style="margin-left: 10px;" compact />
+              </sui-form-field>
+            </sui-form>
           </sui-tab-pane>
         </sui-tab>
       </sui-grid-column>
