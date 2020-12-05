edecefa3a5dc7536c9b08cddc39b27dcfb7b6702
Ben Szymanski <bszyman@icloud.com>
2020-12-05 14:09:09 -0600

    Complete vpn module layouts

--- src/Modules/VPN/VpnConnections.vue
+++ src/Modules/VPN/VpnConnections.vue
@@ -7,7 +7,22 @@
     </sui-grid-row>
     <sui-grid-row>
       <sui-grid-column :width="16">
-
+        <sui-segment></sui-segment>
+        <sui-table basic="very">
+          <sui-table-body>
+            <sui-table-row>
+              <sui-table-cell text-align="center">
+                <p>L2TP Connections: 0</p>
+              </sui-table-cell>
+              <sui-table-cell text-align="center">
+                <p>PTPP Connections: 0</p>
+              </sui-table-cell>
+              <sui-table-cell text-align="center">
+                <p>Total Connections: 0</p>
+              </sui-table-cell>
+            </sui-table-row>
+          </sui-table-body>
+        </sui-table>
       </sui-grid-column>
     </sui-grid-row>
   </sui-grid>
--- src/Modules/VPN/VpnSettings.vue
+++ src/Modules/VPN/VpnSettings.vue
@@ -9,13 +9,77 @@
       <sui-grid-column :width="16">
         <sui-tab>
           <sui-tab-pane title="General">
-            <h3>General</h3>
+            <sui-form>
+              <sui-form-field>
+                <sui-checkbox label="Enable L2TP over IPSec" />
+              </sui-form-field>
+              <div style="margin-left: 50px;">
+                <sui-form-field inline>
+                  <sui-checkbox label="Restrict access to group:" />
+                  <input type="text" />
+                </sui-form-field>
+                <sui-form-field inline>
+                  <label>IPSec shared secret</label>
+                  <input type="text" />
+                </sui-form-field>
+                <sui-form-field inline>
+                  <label>Starting IP address:</label>
+                  <input type="text" />
+                </sui-form-field>
+                <sui-form-field inline>
+                  <label>Ending IP address:</label>
+                  <input type="text" />
+                </sui-form-field>
+              </div>
+
+              <sui-form-field>
+                <sui-checkbox label="Enable PPTP" />
+              </sui-form-field>
+              <div style="margin-left: 50px;">
+                <sui-form-field>
+                  <sui-checkbox label="Allow 40-bit encryption keys in addition to 128-bit" />
+                </sui-form-field>
+                <sui-form-field inline>
+                  <sui-checkbox label="Restrict access to group:" />
+                  <input type="text" />
+                </sui-form-field>
+                <sui-form-field inline>
+                  <label>Starting IP address:</label>
+                  <input type="text" />
+                </sui-form-field>
+                <sui-form-field inline>
+                  <label>Ending IP address:</label>
+                  <input type="text" />
+                </sui-form-field>
+              </div>
+            </sui-form>
           </sui-tab-pane>
           <sui-tab-pane title="Logging">
-            <h3>Logging</h3>
+            <sui-form>
+              <sui-form-field>
+                <sui-checkbox label="Verbose logging" />
+              </sui-form-field>
+            </sui-form>
           </sui-tab-pane>
           <sui-tab-pane title="Client Information">
-            <h3>Client Information</h3>
+            <sui-form>
+              <sui-form-field>
+                <label>IPSec shared Secret:</label>
+                <textarea rows="3"></textarea>
+              </sui-form-field>
+              <sui-form-field>
+                <label>Search Domains:</label>
+                <textarea rows="3"></textarea>
+              </sui-form-field>
+
+              <h4>Network Routing Definition:</h4>
+              <sui-segment></sui-segment>
+              <div is="sui-button-group" style="margin-bottom: 15px;">
+                <sui-button icon="plus" compact />
+                <sui-button icon="minus" compact />
+                <sui-button icon="pencil alternate" compact />
+              </div>
+            </sui-form>
           </sui-tab-pane>
         </sui-tab>
       </sui-grid-column>
