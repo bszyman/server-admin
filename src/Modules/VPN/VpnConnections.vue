1f20329ccf75e53d555a3bec6cf873d7547897b4
Ben Szymanski <bszyman@icloud.com>
2020-12-11 20:55:01 -0600

    VPN updates for 10.4

--- src/Modules/VPN/VpnConnections.vue
+++ src/Modules/VPN/VpnConnections.vue
@@ -7,7 +7,29 @@
     </sui-grid-row>
     <sui-grid-row>
       <sui-grid-column :width="16">
-        <sui-segment></sui-segment>
+        <div class="table-container" style="margin-top: 20px;">
+          <sui-table compact selectable padded basic="very">
+            <sui-table-header>
+              <sui-table-row>
+                <sui-table-header-cell>User Name</sui-table-header-cell>
+                <sui-table-header-cell>Remote IP Address</sui-table-header-cell>
+                <sui-table-header-cell>Internal IP Address</sui-table-header-cell>
+                <sui-table-header-cell>Protocol</sui-table-header-cell>
+                <sui-table-header-cell>Connected For</sui-table-header-cell>
+              </sui-table-row>
+            </sui-table-header>
+            <sui-table-body>
+              <sui-table-row v-for="c in connections" :key="c.id">
+                <sui-table-cell>{{ c.username }}</sui-table-cell>
+                <sui-table-cell>{{ c.remoteIPAddress }}</sui-table-cell>
+                <sui-table-cell>{{ c.internalIPAddress }}</sui-table-cell>
+                <sui-table-cell>{{ c.protocol }}</sui-table-cell>
+                <sui-table-cell>{{ c.connectedFor }}</sui-table-cell>
+              </sui-table-row>
+            </sui-table-body>
+          </sui-table>
+        </div>
+
         <sui-table basic="very">
           <sui-table-body>
             <sui-table-row>
@@ -32,7 +54,12 @@
 import VpnMenu from "@/Modules/VPN/Components/VpnMenu";
 export default {
   name: "VpnConnections",
-  components: {VpnMenu}
+  components: {VpnMenu},
+  data() {
+    return {
+      connections: []
+    }
+  }
 }
 </script>
 
--- src/Modules/VPN/VpnSettings.vue
+++ src/Modules/VPN/VpnSettings.vue
@@ -8,20 +8,12 @@
     <sui-grid-row>
       <sui-grid-column :width="16">
         <sui-tab>
-          <sui-tab-pane title="General">
+          <sui-tab-pane title="L2TP">
             <sui-form>
               <sui-form-field>
                 <sui-checkbox label="Enable L2TP over IPSec" />
               </sui-form-field>
               <div style="margin-left: 50px;">
-                <sui-form-field inline>
-                  <sui-checkbox label="Restrict access to group:" />
-                  <input type="text" />
-                </sui-form-field>
-                <sui-form-field inline>
-                  <label>IPSec shared secret</label>
-                  <input type="text" />
-                </sui-form-field>
                 <sui-form-field inline>
                   <label>Starting IP address:</label>
                   <input type="text" />
@@ -30,8 +22,37 @@
                   <label>Ending IP address:</label>
                   <input type="text" />
                 </sui-form-field>
-              </div>
+                <sui-form-field inline>
+                  <label>PPP Authentication:</label>
+                  <sui-dropdown
+                      placeholder="PPP Authentication"
+                      selection
+                      :options="pppAuthenticationOptions"
+                      v-model="selectedPPPAuthenticationOption"
+                  />
+                </sui-form-field>
 
+                <h4>IPSec Authentication</h4>
+                <div style="margin-left: 25px;">
+                  <sui-form-field inline>
+                    <sui-checkbox radio label="Shared Secret:" />
+                    <input type="text" />
+                  </sui-form-field>
+                  <sui-form-field inline>
+                    <sui-checkbox radio label="Certificate:" />
+                    <sui-dropdown
+                        placeholder="Certificate"
+                        selection
+                        :options="certificateOptions"
+                        v-model="selectedCertificateOption"
+                    />
+                  </sui-form-field>
+                </div>
+              </div>
+            </sui-form>
+          </sui-tab-pane>
+          <sui-tab-pane title="PPTP">
+            <sui-form>
               <sui-form-field>
                 <sui-checkbox label="Enable PPTP" />
               </sui-form-field>
@@ -39,18 +60,16 @@
                 <sui-form-field>
                   <sui-checkbox label="Allow 40-bit encryption keys in addition to 128-bit" />
                 </sui-form-field>
-                <sui-form-field inline>
-                  <sui-checkbox label="Restrict access to group:" />
-                  <input type="text" />
-                </sui-form-field>
-                <sui-form-field inline>
-                  <label>Starting IP address:</label>
-                  <input type="text" />
-                </sui-form-field>
-                <sui-form-field inline>
-                  <label>Ending IP address:</label>
-                  <input type="text" />
-                </sui-form-field>
+                <div style="margin-left: 25px;">
+                  <sui-form-field inline>
+                    <label>Starting IP address:</label>
+                    <input type="text" />
+                  </sui-form-field>
+                  <sui-form-field inline>
+                    <label>Ending IP address:</label>
+                    <input type="text" />
+                  </sui-form-field>
+                </div>
               </div>
             </sui-form>
           </sui-tab-pane>
@@ -64,7 +83,7 @@
           <sui-tab-pane title="Client Information">
             <sui-form>
               <sui-form-field>
-                <label>IPSec shared Secret:</label>
+                <label>DNS Servers:</label>
                 <textarea rows="3"></textarea>
               </sui-form-field>
               <sui-form-field>
@@ -73,7 +92,24 @@
               </sui-form-field>
 
               <h4>Network Routing Definition:</h4>
-              <sui-segment></sui-segment>
+              <div class="table-container" style="height: 225px;">
+                <sui-table compact selectable padded basic="very">
+                  <sui-table-header>
+                    <sui-table-row>
+                      <sui-table-header-cell>Network Address</sui-table-header-cell>
+                      <sui-table-header-cell>Network Mask</sui-table-header-cell>
+                      <sui-table-header-cell>Network Type</sui-table-header-cell>
+                    </sui-table-row>
+                  </sui-table-header>
+                  <sui-table-body>
+                    <sui-table-row v-for="n in nrd" :key="n.id">
+                      <sui-table-cell>{{ n.networkAddress }}</sui-table-cell>
+                      <sui-table-cell>{{ n.networkMask }}</sui-table-cell>
+                      <sui-table-cell>{{ n.networkType }}</sui-table-cell>
+                    </sui-table-row>
+                  </sui-table-body>
+                </sui-table>
+              </div>
               <div is="sui-button-group" style="margin-bottom: 15px;">
                 <sui-button icon="plus" compact />
                 <sui-button icon="minus" compact />
@@ -91,7 +127,24 @@
 import VpnMenu from "@/Modules/VPN/Components/VpnMenu";
 export default {
   name: "VpnSettings",
-  components: {VpnMenu}
+  components: {VpnMenu},
+  data() {
+    const pppAuthenticationOptions = [
+      {key: 1, text: "MS-CHAPv2", value: 1},
+    ];
+
+    const certificateOptions = [
+      {key: 1, text: "No Certificate", value: 1},
+    ];
+
+    return {
+      pppAuthenticationOptions: pppAuthenticationOptions,
+      selectedPPPAuthenticationOption: 1,
+      certificateOptions: certificateOptions,
+      selectedCertificateOption: 1,
+      nrd: []
+    }
+  }
 }
 </script>
 
